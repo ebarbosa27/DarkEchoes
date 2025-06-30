07fb39ac9b7e5c912268463c5bf5b8b23c5f312f
ebarbosa27 <ebarbosa5449@gmail.com>
2025-06-29 19:42:58 -0500

    added a skeleton of how the dom will display data

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,18 @@
 export default function App() {
   // TODO
+  return (
+    <>
+      <h1>Dark Echoes</h1>
+      <div>
+        <div>
+          <h2>Episodes</h2>
+          {/* List items here */}
+        </div>
+        <div>
+          <h2>{/* Selected Episode */}</h2>
+          {/* selected episode data */}
+        </div>
+      </div>
+    </>
+  );
 }
